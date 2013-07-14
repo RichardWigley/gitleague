f600169299408add69f3ff1020e897e2aa9c7c45
Richard Wigley <Richard.Wigley@gmail.com>
2013-07-14 21:39:52 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitleague::Application.config.secret_key_base = '28dd0265bad79a3308c9e42793abdeac7243b7f757c445551f099c2f398687a6e1c061a40a71497b8f6b150a9fe43136a40d0bab33794551c07919162c893d82'
