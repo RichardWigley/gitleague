0eb8ccae1ea07aae438e5a92cb65c6fc79a0abb8
Richard Wigley <Richard.Wigley@gmail.com>
2013-07-15 18:45:00 +0100

    Importing users from GitHub

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
   has_many :events
+
+  def self.import name, klass = ::Github::Client
+    records = klass.fetch 'tenderlove'
+
+    records.each do |record|
+      actor = Actor.find_or_create_by(name: 'tenderlove')
+      Event.create_from_record actor, record
+    end
+  end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
+require_relative '../../lib/github/client'
 
 describe Actor do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "import " do
+    it "adds actors" do
+      records = Github::Client.fetch 'tenderlove'
+      expect{Actor.import 'tenderlove', Github::Client}.to change(Actor, :count).by(1)
+    end
+
+    it "adds events" do
+      records = Github::Client.fetch 'tenderlove'
+      expect{Actor.import 'tenderlove', Github::Client}.to change(Event, :count).by(records.length)
+    end
+  end
 end
+
